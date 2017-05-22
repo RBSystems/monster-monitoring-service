94d85b7ce4ba2b66eff97eaaea9886f4ee663c2c
Clark Green <clark_green@byu.edu>
2017-05-22 10:55:16 -0600

    Obi-Wan never told you what happened to your father

--- helpers/room.go
+++ helpers/room.go
@@ -6,3 +6,7 @@ import "github.com/byuoitav/av-api/base"
 func QueryRoomStatus(building string, room string) (base.PublicRoom, error) {
 	return base.PublicRoom{}, nil
 }
+
+func GetRoomInfo(building string, room string) ([]byte, error) {
+	return []byte{}, nil
+}
