2c1137d92754646970b9495c5307eb49648ebd0f
Clark Green <clark_green@byu.edu>
2017-05-22 10:57:14 -0600

    He told me enough

--- helpers/room.go
+++ helpers/room.go
@@ -2,11 +2,12 @@ package helpers
 
 import "github.com/byuoitav/av-api/base"
 
-//Gets the current status of a room from the AV API
+//queries the data store and returns a PublicRom
 func QueryRoomStatus(building string, room string) (base.PublicRoom, error) {
 	return base.PublicRoom{}, nil
 }
 
+//queries the data store and dumps room info
 func GetRoomInfo(building string, room string) ([]byte, error) {
 	return []byte{}, nil
 }
